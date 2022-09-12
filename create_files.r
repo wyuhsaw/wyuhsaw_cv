ee4881155e3c7d974640fbdd17b6d827c2716c58
Woei-Yuh Saw <106300914+wyuhsaw@users.noreply.github.com>
2022-09-12 12:57:58 +1000

    Update create_files.r
    deleted output and file location

--- create_files.r
+++ create_files.r
@@ -1,9 +1,9 @@
 datadrivencv::use_datadriven_cv(
   full_name = "Woei-Yuh Saw",
   data_location = "",
-  pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  html_location = "https://www.ruizsergio.com/cv/",
-  source_location = "https://github.com/nstrayer/cv",
+  pdf_location = "",
+  html_location = "",
+  source_location = "",
   open_files = FALSE,
   # use_network_logo = FALSE
 )
